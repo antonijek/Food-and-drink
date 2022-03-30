4d5d3b28854c50d10f37adf9ca0c38c641f27d93
eris00 <erissutkovic00@gmail.com>
2022-03-30 22:01:45 +0200

    updated files

--- src/list.js
+++ src/list.js
@@ -1,3 +1,5 @@
+let containerHeader = document.querySelector(".container");
+containerHeader.appendChild(header.content);
 
 let containerFooter = document.querySelector(".container");
 containerFooter.appendChild(footer.content);
