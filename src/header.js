8a75199c99851c81ebd2fca5304a479b470f6498
eris00 <erissutkovic00@gmail.com>
2022-04-15 14:42:55 +0200

    header, active links

--- src/header.js
+++ src/header.js
@@ -11,16 +11,16 @@ header.innerHTML = `
 
     <nav>
       <ul>
-        <li><a href="../index.html">Home</a></li>
-        <li><a href="../html/list.html" id="list-a">List</a>
+        <li><a class="current" href="../index.html">Home</a></li>
+        <li><a class="current" href="../html/list.html" id="list-a">List</a>
           <ul>
             <li><a class="regional" href="#">Regional</a></li>
             <li><a class="large" href="#">Large</a></li>
             <li><a class="bar" href="#">Bar</a></li>
           </ul>
         </li>
-        <li><a href="#">About us</a></li>
-        <li><a href="../html/contact-us.html">Contact us</a></li>
+        <li><a class="current" href="../html/about-us.html">About us</a></li>
+        <li><a class="current" href="../html/contact-us.html">Contact us</a></li>
       </ul>
     </nav>
 
@@ -107,5 +107,13 @@ let hamDropdwnDiv = document.getElementById('ham-dropdwn-div');
 
 
 listArrow.addEventListener('click', () => { hamDropdwnDiv.classList.toggle('hide'); })
+
+const ActivePage = window.location.pathname;
+const navLinks = document.querySelectorAll('.current')
+.forEach(link => {
+  if(link.href.includes(`${ActivePage}`)) {
+    link.classList.add('active');
+  }
+});
 
 
